b6017478bda2f42b8f5a092b91ad81d795dce4f9
vl3c00 <carlosalec8969@gmail.com>
2025-01-28 18:53:36 +0200

    first commit

--- budget-tracker/components/Navbar.tsx
+++ budget-tracker/components/Navbar.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import React from "react";
-import Logo from "@/components/Logo";
+import React, { useState } from "react";
+import Logo, { LogoMobile } from "@/components/Logo";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 import {ThemeSwitcherBtn} from "./ThemeSwitcherBtn"
 import { UserButton } from "@clerk/nextjs";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+
 
 function Navbar() {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 }
@@ -22,6 +26,41 @@ const items = [
   { label: "Transactions", link: "/transactions" },
   { label: "Manage", link: "/manage" },
 ];
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+          <Button variant={"ghost"} size={"icon"}>
+            <Menu />
+          </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+            <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => ( <NavbarItem key={item.label}
+              link={item.link}
+              label={item.label}
+               />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 function DesktopNavbar() {
   return (
