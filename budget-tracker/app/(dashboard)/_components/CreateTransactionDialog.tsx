d0ba7075f52ac62d0fd1390007b8326ce6ee2dfb
vl3c00 <carlosalec8969@gmail.com>
2025-01-30 16:48:40 +0200

    first commit

---
+++ budget-tracker/app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
+import {ReactNode} from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface Props {
+    trigger: ReactNode;
+    type: TransactionType;
+}
+
+import React from 'react';
+
+function CreateTransactionDialog({trigger, type}: Props) {
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+            type,
+            date: new Date(),
+        },
+    });
+  return (
+<Dialog>
+    <DialogTrigger asChild>{trigger}</DialogTrigger>
+    <DialogContent>
+        <DialogHeader>
+            <DialogTitle>
+                Create a new {" "} 
+                <span className={cn(
+                    "m-1",
+                    type === "income" ? "text-emarald-500" : "text-red-500" 
+                )}
+                >
+                    {type}
+                </span>
+                transaction
+            </DialogTitle>
+        </DialogHeader>
+    </DialogContent>
+</Dialog>
+  );
+}
+
+export default CreateTransactionDialog;
