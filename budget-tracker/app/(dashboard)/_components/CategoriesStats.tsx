f9b8d6e4494f3055cf9f0a975c898b8a4f382d9d
vl3c00 <carlosalec8969@gmail.com>
2025-02-03 21:24:10 +0200

    categoriesStats2:58:39

--- budget-tracker/app/(dashboard)/_components/CategoriesStats.tsx
+++ budget-tracker/app/(dashboard)/_components/CategoriesStats.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { GetCategoriesStatsResponseType } from "@/app/api/stats/categories/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
+import { TransactionType } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import React, { useMemo } from "react";
@@ -12,7 +16,7 @@ interface Props {
 }
 
 function CategoriesStats({userSettings, from, to}: Props) {
-  const statsQuery = useQuery({
+  const statsQuery = useQuery<GetCategoriesStatsResponseType>({
     queryKey: ["overview", "stats", "categories", from, to],
     queryFn: () => fetch(`/api/stats/categories?from=${DateToUTCDate(from)}&to=${DateToUTCDate(
       to
@@ -25,7 +29,46 @@ function CategoriesStats({userSettings, from, to}: Props) {
   }, [userSettings.currency]);
 
 
-  return <div>hbhbhb</div>;
+  return <div className="flex w-full flex-wrap gap-2 md:flex-nowrap">
+    <SkeletonWrapper isLoading={statsQuery.isFetching}>
+      <CategoriesCard
+      formatter={formatter}
+      type="income"
+      data={statsQuery.data || []}
+      />
+    </SkeletonWrapper>
+  </div>;
 }
 
 export default CategoriesStats;
+
+function CategoriesCard({data, type, formatter}:{
+  type: TransactionType,
+  formatter: Intl.NumberFormat,
+  data: GetCategoriesStatsResponseType
+}) {
+  const filteredData = data.filter((el) => el.type === type);
+  const total = filteredData.reduce((acc, el) => acc + (el._sum?.amount || 0),
+  0
+);
+
+return (
+  <Card className="h-80 w-full col-span-6">
+    <CardHeader>
+      <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col"></CardTitle>
+    </CardHeader>
+
+    <div className="flex items-center justify-between gap-2">
+      {filteredData.length === 0 && (
+        <div className="flex h-60 w-full flex-col items-center justify-center">
+          No data for the selected period
+          <p className="text-sm text-muted-foreground">
+            Try selecting a different period or try adding new {" "}
+            {type === "income" ? "incomes" : "expenses"}
+          </p>
+        </div>
+      )}
+    </div>
+  </Card>
+);
+}
