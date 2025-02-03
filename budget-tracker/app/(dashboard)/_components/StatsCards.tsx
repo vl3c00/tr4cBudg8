c923683cf87b17e7243f3dc2111ea515654f54bf
vl3c00 <carlosalec8969@gmail.com>
2025-02-03 16:01:19 +0200

    first commit

--- budget-tracker/app/(dashboard)/_components/StatsCards.tsx
+++ budget-tracker/app/(dashboard)/_components/StatsCards.tsx
@@ -2,11 +2,12 @@
 
 import { GetBalanceStatsResponseType } from "@/app/api/stats/balance/routes";
 import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { Card } from "@/components/ui/card";
 import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import { TrendingUp } from "lucide-react";
-import React, { ReactNode, useMemo } from 'react'
+import React, { ReactNode, useCallback, useMemo } from 'react'
 
 interface Props {
     from: Date;
@@ -34,7 +35,7 @@ function StatsCards({ from, to, userSettings}: Props) {
   return (
     <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
       <SkeletonWrapper isLoading={statsQuery.isFetching}>
-        <StatsCard
+        <StatCard
         formatter={formatter}
         value={income}
         title="Income"
@@ -56,4 +57,18 @@ function StatCard({formatter, value, title, icon}: {
   icon: ReactNode;
   title: String;
   value: number;
-}) {}
+}) {
+  const formatFn = useCallback((value:number) => {
+    return formatter.format(value);
+  }, [formatter]
+);
+
+return (
+  <Card className="flex h-24 w-full items-center gap-2 p-4">
+    {icon}
+    <div className="flex flex-col items-center gap-0">
+      <p className="text-muted-foreground"></p>
+    </div>
+  </Card>
+)
+}
